4de3b304343327c3240898cb784ce3a34b0ea6fa
Petar Ojdrovic <Petar-PC@Petars-MacBook-Pro.local>
2017-05-15 18:14:40 -0400

    Initial commit with storyboards

---
+++ FoodWiz/RecipesViewController.swift
@@ -0,0 +1,36 @@
+//
+//  RecipesViewController.swift
+//  FoodWiz
+//
+//  Created by Petar Ojdrovic on 5/11/17.
+//  Copyright © 2017 Petar Ojdrovic. All rights reserved.
+//
+
+import UIKit
+
+class RecipesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Loaded the recipes page!")
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FoodWiz/dbManager.swift
@@ -0,0 +1,9 @@
+//
+//  dbManager.swift
+//  FoodWiz
+//
+//  Created by Petar Ojdrovic on 5/11/17.
+//  Copyright © 2017 Petar Ojdrovic. All rights reserved.
+//
+
+import Foundation
